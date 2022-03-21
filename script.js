5bfe94dc13a79876e32ba0d1a34d716303577f45
Milos Milijasevic <milosrm20005@gmail.com>
2022-03-21 21:44:47 +0100

    updated

--- script.js
+++ script.js
@@ -10,307 +10,447 @@ const buttonSeven = document.querySelector(`.seven`);
 const buttonEight = document.querySelector(`.eight`);
 const buttonNine = document.querySelector(`.nine`);
 
+
+const buttonPlayAgain = document.querySelector(`.play-again`);
+const buttonNewGame = document.querySelector(`.new-game`);
+
+
+const winnerPlayer = document.querySelector(`.winner-player`);
+const playerScoreX = document.querySelector(`.player-x-score`);
+const playerScoreO = document.querySelector(`.player-o-score`);
+
+
 let player = true;
 let playing = true;
 
+let scoreX = 0;
+let scoreO = 0;
+
+
 buttonOne.addEventListener(`click`, () =>{
-
-    if(buttonOne.textContent.includes(`x`)){
-        return 0;
-    }
-
-    else if(buttonOne.textContent.includes(`o`)){
-        return 0;
-    }
-
-    else if(player && playing){
-        buttonOne.textContent = `x`;
-    }
+    if(playing){
     
-    else if(!player && playing){
-        buttonOne.textContent = `o`;
+        buttonOne.style.color = `black`;
+
+        if(buttonOne.textContent.includes(`x`)){
+            return 0;
+        }
+
+        else if(buttonOne.textContent.includes(`o`)){
+            return 0;
+        }
+
+        else if(player && playing){
+            buttonOne.textContent = `x`;
+        }
+        
+        else if(!player && playing){
+            buttonOne.textContent = `o`;
+        }
+
+        player = !player;
+        
+        winO();
+        winX();
     }
-
-    player = !player;
-
-    winO();
-    winX();
 });
 
 buttonTwo.addEventListener(`click`, () =>{
+    if(playing){
 
-    if(buttonTwo.textContent.includes(`x`)){
-        return 0;
+        buttonTwo.style.color = `black`;
+
+        if(buttonTwo.textContent.includes(`x`)){
+            return 0;
+        }
+
+        else if(buttonTwo.textContent.includes(`o`)){
+            return 0;
+        }
+
+        else if(player && playing){
+            buttonTwo.textContent = `x`;
+        }
+
+        else if(!player && playing){
+            buttonTwo.textContent = `o`;
+        }
+
+        player = !player;
+
+        winO();
+        winX();
     }
-
-    else if(buttonTwo.textContent.includes(`o`)){
-        return 0;
-    }
-
-    else if(player && playing){
-        buttonTwo.textContent = `x`;
-    }
-
-    else if(!player && playing){
-        buttonTwo.textContent = `o`;
-    }
-
-    player = !player;
-
-    winO();
-    winX();
 });
 
 buttonThree.addEventListener(`click`, () =>{
+    if(playing){
 
-    if(buttonThree.textContent.includes(`x`)){
-        return 0;
+        buttonThree.style.color = `black`;
+
+        if(buttonThree.textContent.includes(`x`)){
+            return 0;
+        }
+
+        else if(buttonThree.textContent.includes(`o`)){
+            return 0;
+        }
+
+        else if(player && playing){
+            buttonThree.textContent = `x`;
+        }
+
+        else if(!player && playing){
+            buttonThree.textContent = `o`;
+        }
+
+        player = !player;
+
+        winO();
+        winX();
     }
-
-    else if(buttonThree.textContent.includes(`o`)){
-        return 0;
-    }
-
-    else if(player && playing){
-        buttonThree.textContent = `x`;
-    }
-
-    else if(!player && playing){
-        buttonThree.textContent = `o`;
-    }
-
-    player = !player;
-
-    winO();
-    winX();
 });
 
 buttonFour.addEventListener(`click`, () =>{
+    if(playing){
 
-    if(buttonFour.textContent.includes(`x`)){
-        return 0;
+        buttonFour.style.color = `black`;
+
+        if(buttonFour.textContent.includes(`x`)){
+            return 0;
+        }
+
+        else if(buttonFour.textContent.includes(`o`)){
+            return 0;
+        }
+
+        else if(player && playing){
+            buttonFour.textContent = `x`;
+        }
+
+        else if(!player && playing){
+            buttonFour.textContent = `o`;
+        }
+
+        player = !player;
+
+        winO();
+        winX();
     }
-
-    else if(buttonFour.textContent.includes(`o`)){
-        return 0;
-    }
-
-    else if(player && playing){
-        buttonFour.textContent = `x`;
-    }
-
-    else if(!player && playing){
-        buttonFour.textContent = `o`;
-    }
-
-    player = !player;
-
-    winO();
-    winX();
 });
 
 buttonFive.addEventListener(`click`, () =>{
+    if(playing){
 
-    if(buttonFive.textContent.includes(`x`)){
-        return 0;
+        buttonFive.style.color = `black`;
+
+        if(buttonFive.textContent.includes(`x`)){
+            return 0;
+        }
+
+        else if(buttonFive.textContent.includes(`o`)){
+            return 0;
+        }
+
+        else if(player && playing){
+            buttonFive.textContent = `x`;
+        }
+
+        else if(!player && playing){
+            buttonFive.textContent = `o`;
+        }
+
+        player = !player;
+
+        winO();
+        winX();
     }
-
-    else if(buttonFive.textContent.includes(`o`)){
-        return 0;
-    }
-
-    else if(player && playing){
-        buttonFive.textContent = `x`;
-    }
-
-    else if(!player && playing){
-        buttonFive.textContent = `o`;
-    }
-
-    player = !player;
-
-    winO();
-    winX();
 });
 
 buttonSix.addEventListener(`click`, () =>{
+    if(playing){
 
-    if(buttonSix.textContent.includes(`x`)){
-        return 0;
+        buttonSix.style.color = `black`;
+
+        if(buttonSix.textContent.includes(`x`)){
+            return 0;
+        }
+
+        else if(buttonSix.textContent.includes(`o`)){
+            return 0;
+        }
+
+        else if(player && playing){
+            buttonSix.textContent = `x`;
+        }
+
+        else if(!player && playing){
+            buttonSix.textContent = `o`;
+        }
+
+        player = !player;
+
+        winO();
+        winX();
     }
-
-    else if(buttonSix.textContent.includes(`o`)){
-        return 0;
-    }
-
-    else if(player && playing){
-        buttonSix.textContent = `x`;
-    }
-
-    else if(!player && playing){
-        buttonSix.textContent = `o`;
-    }
-
-    player = !player;
-
-    winO();
-    winX();
 });
 
 buttonSeven.addEventListener(`click`, () =>{
+    if(playing){
 
-    if(buttonSeven.textContent.includes(`x`)){
-        return 0;
+        buttonSeven.style.color = `black`;
+
+        if(buttonSeven.textContent.includes(`x`)){
+            return 0;
+        }
+
+        else if(buttonSeven.textContent.includes(`o`)){
+            return 0;
+        }
+
+        else if(player && playing){
+            buttonSeven.textContent = `x`;
+        }
+
+        else if(!player && playing){
+            buttonSeven.textContent = `o`;
+        }
+
+        player = !player;
+
+        winO();
+        winX();
     }
-
-    else if(buttonSeven.textContent.includes(`o`)){
-        return 0;
-    }
-
-    else if(player && playing){
-        buttonSeven.textContent = `x`;
-    }
-
-    else if(!player && playing){
-        buttonSeven.textContent = `o`;
-    }
-
-    player = !player;
-
-    winO();
-    winX();
 });
 
 buttonEight.addEventListener(`click`, () =>{
+    if(playing){
 
-    if(buttonEight.textContent.includes(`x`)){
-        return 0;
+        buttonEight.style.color = `black`;
+
+        if(buttonEight.textContent.includes(`x`)){
+            return 0;
+        }
+
+        else if(buttonEight.textContent.includes(`o`)){
+            return 0;
+        }
+
+        else if(player && playing){
+            buttonEight.textContent = `x`;
+        }
+
+        else if(!player && playing){
+            buttonEight.textContent = `o`;
+        }
+
+        player = !player;
+
+        winO();
+        winX();
     }
-
-    else if(buttonEight.textContent.includes(`o`)){
-        return 0;
-    }
-
-    else if(player && playing){
-        buttonEight.textContent = `x`;
-    }
-
-    else if(!player && playing){
-        buttonEight.textContent = `o`;
-    }
-
-    player = !player;
-
-    winO();
-    winX();
 });
 
 buttonNine.addEventListener(`click`, () =>{
+    if(playing){
 
-    if(buttonNine.textContent.includes(`x`)){
-        return 0;
+        buttonNine.style.color = `black`;
+
+        if(buttonNine.textContent.includes(`x`)){
+            return 0;
+        }
+
+        else if(buttonNine.textContent.includes(`o`)){
+            return 0;
+        }
+
+        else if(player && playing){
+            buttonNine.textContent = `x`;
+        }
+
+        else if(!player && playing){
+            buttonNine.textContent = `o`;
+        }
+
+        player = !player;
+
+        winO();
+        winX();
     }
-
-    else if(buttonNine.textContent.includes(`o`)){
-        return 0;
-    }
-
-    else if(player && playing){
-        buttonNine.textContent = `x`;
-    }
-
-    else if(!player && playing){
-        buttonNine.textContent = `o`;
-    }
-
-    player = !player;
-
-    winO();
-    winX();
 });
+
 
 const winO = function (){
     if(buttonOne.textContent.includes(`o`) && buttonTwo.textContent.includes(`o`) && buttonThree.textContent.includes(`o`)){
-        console.log(`WIN`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player o wins !`;
+        scoreO++;
+        playerScoreO.textContent = scoreO;
     }
 
     else if(buttonOne.textContent.includes(`o`) && buttonFour.textContent.includes(`o`) && buttonSeven.textContent.includes(`o`)){
-        console.log(`WIN2`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player o wins !`;
+        scoreO++;
+        playerScoreO.textContent = scoreO;  
     }
 
     else if(buttonSeven.textContent.includes(`o`) && buttonEight.textContent.includes(`o`) && buttonNine.textContent.includes(`o`)){
-        console.log(`WIN3`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player o wins !`;
+        scoreO++;
+        playerScoreO.textContent = scoreO;  
     }
 
     else if(buttonThree.textContent.includes(`o`) && buttonSix.textContent.includes(`o`) && buttonNine.textContent.includes(`o`)){
-        console.log(`WIN4`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player o wins !`; 
+        scoreO++;
+        playerScoreO.textContent = scoreO; 
     }
 
     else if(buttonFour.textContent.includes(`o`) && buttonFive.textContent.includes(`o`) && buttonSix.textContent.includes(`o`)){
-        console.log(`WIN5`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player o wins !`; 
+        scoreO++;
+        playerScoreO.textContent = scoreO;
     }
 
     else if(buttonTwo.textContent.includes(`o`) && buttonFive.textContent.includes(`o`) && buttonEight.textContent.includes(`o`)){
-        console.log(`WIN6`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player o wins !`;  
+        scoreO++;
+        playerScoreO.textContent = scoreO;
     }
 
     else if(buttonOne.textContent.includes(`o`) && buttonFive.textContent.includes(`o`) && buttonNine.textContent.includes(`o`)){
-        console.log(`WIN7`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player o wins !`; 
+        scoreO++;
+        playerScoreO.textContent = scoreO; 
     }
 
     else if(buttonThree.textContent.includes(`o`) && buttonFive.textContent.includes(`o`) && buttonSeven.textContent.includes(`o`)){
-        console.log(`WIN8`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player o wins !`; 
+        scoreO++;
+        playerScoreO.textContent = scoreO; 
     }
      
 };
 
 const winX = function (){
     if(buttonOne.textContent.includes(`x`) && buttonTwo.textContent.includes(`x`) && buttonThree.textContent.includes(`x`)){
-        console.log(`WIN`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player x wins !`;
+        scoreX++;
+        playerScoreX.textContent = scoreX;  
     }
 
     else if(buttonOne.textContent.includes(`x`) && buttonFour.textContent.includes(`x`) && buttonSeven.textContent.includes(`x`)){
-        console.log(`WIN2`);
         playing = false; 
+        winnerPlayer.textContent = `Player x wins !`;
+        scoreX++;
+        playerScoreX.textContent = scoreX; 
     }
 
     else if(buttonSeven.textContent.includes(`x`) && buttonEight.textContent.includes(`x`) && buttonNine.textContent.includes(`x`)){
-        console.log(`WIN3`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player x wins !`;
+        scoreX++;
+        playerScoreX.textContent = scoreX;  
     }
 
     else if(buttonThree.textContent.includes(`x`) && buttonSix.textContent.includes(`x`) && buttonNine.textContent.includes(`x`)){
-        console.log(`WIN4`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player x wins !`;
+        scoreX++;
+        playerScoreX.textContent = scoreX;  
     }
 
     else if(buttonFour.textContent.includes(`x`) && buttonFive.textContent.includes(`x`) && buttonSix.textContent.includes(`x`)){
-        console.log(`WIN5`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player x wins !`;
+        scoreX++;
+        playerScoreX.textContent = scoreX;  
     }
 
     else if(buttonTwo.textContent.includes(`x`) && buttonFive.textContent.includes(`x`) && buttonEight.textContent.includes(`x`)){
-        console.log(`WIN6`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player x wins !`;
+        scoreX++;
+        playerScoreX.textContent = scoreX;  
     }
 
     else if(buttonOne.textContent.includes(`x`) && buttonFive.textContent.includes(`x`) && buttonNine.textContent.includes(`x`)){
-        console.log(`WIN7`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player x wins !`;
+        scoreX++;
+        playerScoreX.textContent = scoreX;  
     }
 
     else if(buttonThree.textContent.includes(`x`) && buttonFive.textContent.includes(`x`) && buttonSeven.textContent.includes(`x`)){
-        console.log(`WIN8`);
-        playing = false; 
+        playing = false;
+        winnerPlayer.textContent = `Player x wins !`;
+        scoreX++;
+        playerScoreX.textContent = scoreX;  
     }
      
 };
+
+buttonNewGame.addEventListener(`click`, () =>{
+    buttonOne.textContent = `q`;
+    buttonTwo.textContent = `q`;
+    buttonThree.textContent = `q`;
+    buttonFour.textContent = `q`;
+    buttonFive.textContent = `q`;
+    buttonSix.textContent = `q`;
+    buttonSeven.textContent = `q`;
+    buttonEight.textContent = `q`;
+    buttonNine.textContent = `q`;
+
+    buttonOne.style.color = `white`;
+    buttonTwo.style.color = `white`;
+    buttonThree.style.color = `white`;
+    buttonFour.style.color = `white`;
+    buttonFive.style.color = `white`;
+    buttonSix.style.color = `white`;
+    buttonSeven.style.color = `white`;
+    buttonEight.style.color = `white`;
+    buttonNine.style.color = `white`;
+
+    playerScoreX.textContent = 0;
+    playerScoreO.textContent = 0;
+
+    winnerPlayer.textContent = `Who will win ?!`;
+
+    playing = true;
+    scoreO = 0;
+    scoreX = 0;
+});
+
+buttonPlayAgain.addEventListener(`click`, () =>{
+    buttonOne.textContent = `q`;
+    buttonTwo.textContent = `q`;
+    buttonThree.textContent = `q`;
+    buttonFour.textContent = `q`;
+    buttonFive.textContent = `q`;
+    buttonSix.textContent = `q`;
+    buttonSeven.textContent = `q`;
+    buttonEight.textContent = `q`;
+    buttonNine.textContent = `q`;
+
+    buttonOne.style.color = `white`;
+    buttonTwo.style.color = `white`;
+    buttonThree.style.color = `white`;
+    buttonFour.style.color = `white`;
+    buttonFive.style.color = `white`;
+    buttonSix.style.color = `white`;
+    buttonSeven.style.color = `white`;
+    buttonEight.style.color = `white`;
+    buttonNine.style.color = `white`;
+
+    winnerPlayer.textContent = `Who will win ?!`;
+
+    playing = true;
+});
